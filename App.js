24acd82bd9efd6f4f7f84a474c6f86d6cdea9451
mikaelbraghini <mikael.braghini@gmail.com>
2025-03-21 21:00:20 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js  
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "./styles"; // Importa os estilos definidos em styles.js  
 
+// Componente principal App  
 export default function App() {
   return (
+    // View principal que serve como container, aplicando os estilos definidos em styles.container  
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box}>
+        {/* Texto dentro da caixa, aplicando os estilos definidos em styles.boxText */}
+        <Text style={styles.boxText}>1</Text>
+      </View>
+      {/* View que representa uma caixa, aplicando os estilos definidos em styles.box */}
+      <View style={styles.box}>
+        {/* Texto dentro da caixa, aplicando os estilos definidos em styles.boxText */}
+        <Text style={styles.boxText}>2</Text>
+      </View>
+      {/* View que representa uma caixa, aplicando os estilos definidos em styles.box */}
+      <View style={styles.box}>
+        {/* Texto dentro da caixa, aplicando os estilos definidos em styles.boxText */}
+        <Text style={styles.boxText}>3</Text>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
